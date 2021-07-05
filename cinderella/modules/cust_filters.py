7ba1d4a938e493a4ce10aa58f16176d9a9801fce
Sathishzus <73419683+Sathishzus@users.noreply.github.com>
2021-07-05 20:03:11 +0530

    Update cust_filters.py

--- cinderella/modules/cust_filters.py
+++ cinderella/modules/cust_filters.py
@@ -297,18 +297,18 @@ def __chat_settings__(chat_id, user_id):
 
 
 __help__ = """
- - /filters: list all active filters in this chat.
+ 🔱 /filters: list all active filters in this chat.
 
 *Admin only:*
- - /filter <keyword> <reply message>: add a filter to this chat. The bot will now reply that message whenever 'keyword'\
+ 🔱 /filter <keyword> <reply message>: add a filter to this chat. The bot will now reply that message whenever 'keyword'\
 is mentioned. If you reply to a sticker with a keyword, the bot will reply with that sticker. NOTE: all filter \
 keywords are in lowercase. If you want your keyword to be a sentence, use quotes. eg: /filter "hey there" How you \
 doin?
- - /stop <filter keyword>: stop that filter.
- - /stopall: stop all filters
+ 🔱 /stop <filter keyword>: stop that filter.
+ 🔱 /stopall: stop all filters
 """
 
-__mod_name__ = "FILTERS"
+__mod_name__ = "♻️ FILTERS ♻️"
 
 FILTER_HANDLER = CommandHandler("filter", filters)
 STOP_HANDLER = CommandHandler("stop", stop_filter)
