524d31abd8d63f41def3ec2acf3c35bd907f2be1
Sathishzus <73419683+Sathishzus@users.noreply.github.com>
2021-07-05 12:44:19 +0530

    Update time.py

--- cinderella/modules/time.py
+++ cinderella/modules/time.py
@@ -5,7 +5,7 @@ import requests
 from cinderella import TIME_API_KEY, dispatcher
 from cinderella.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
-from telegram.ext import CallbackContext, run_async
+from telegram.ext import run_async
 
 
 def generate_time(to_find: str, findtype: List[str]) -> str:
