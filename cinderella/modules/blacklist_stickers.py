a2ad472e276bf4555988ff3a913e9fbc6e48f9b1
Sathishzus <73419683+Sathishzus@users.noreply.github.com>
2021-07-05 19:27:44 +0530

    Update blacklist_stickers.py

--- cinderella/modules/blacklist_stickers.py
+++ cinderella/modules/blacklist_stickers.py
@@ -357,17 +357,17 @@ def __stats__():
 __help__ = """
 Blacklist sticker is used to stop certain stickers. Whenever a sticker is sent, the message will be deleted immediately.
 *NOTE:* Blacklist stickers do not affect the group admin.
- - /blsticker: See current blacklisted sticker.
+ 🔱 /blsticker: See current blacklisted sticker.
 *Only admin:*
- - /addblsticker <sticker link>: Add the sticker trigger to the black list. Can be added via reply sticker.
- - /unblsticker <sticker link>: Remove triggers from blacklist. The same newline logic applies here, so you can delete multiple triggers at once.
- - /rmblsticker <sticker link>: Same as above.
- - /blstickermode ban/tban/mute/tmute .
+ 🔱 /addblsticker <sticker link>: Add the sticker trigger to the black list. Can be added via reply sticker.
+ 🔱 /unblsticker <sticker link>: Remove triggers from blacklist. The same newline logic applies here, so you can delete multiple triggers at once.
+ 🔱 /rmblsticker <sticker link>: Same as above.
+ 🔱 /blstickermode ban/tban/mute/tmute .
 Note:
- - `<sticker link>` can be `https://t.me/addstickers/<sticker>` or just `<sticker>` or reply to the sticker message.
+ 🔱 `<sticker link>` can be `https://t.me/addstickers/<sticker>` or just `<sticker>` or reply to the sticker message.
 """
 
-__mod_name__ = "STICKER BLACKLIST"
+__mod_name__ = "⭕️ STICKER BLACKLIST ⭕️"
 
 BLACKLIST_STICKER_HANDLER = DisableAbleCommandHandler("blsticker", blackliststicker, pass_args=True, admin_ok=True)
 ADDBLACKLIST_STICKER_HANDLER = DisableAbleCommandHandler("addblsticker", add_blackliststicker)
